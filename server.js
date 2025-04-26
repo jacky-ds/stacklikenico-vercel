43a460d0e4449fbb57b7b84696447e82ff85dc3b
Jacqueline Scheidl <sdjacqueline@gmail.com>
2025-04-26 23:01:16 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ['https://stacklikenicobpc.web.app/', 'http://localhost:5173/'],
+  origin: ['https://stacklikenicobpc.web.app', 'http://localhost:5173'],
 };
 
 app.use(cors(corsOptions));

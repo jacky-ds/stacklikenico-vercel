5298e57443700b94c269218bbb06080e8adba6df
Jacqueline Scheidl <sdjacqueline@gmail.com>
2025-04-26 22:58:25 +0200

    include cors

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-// server.js
 import express from 'express';
+import cors from 'cors';
 import { fetchExchangeRates } from './exchange-rate.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const corsOptions = {
+  origin: ['https://stacklikenicobpc.web.app/', 'http://localhost:5173/'],
+};
+
+app.use(cors(corsOptions));
 
 app.get('/api/exchange-rate', async (req, res) => {
   const result = await fetchExchangeRates();
